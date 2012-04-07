8efc9194d067135a56d442b8a6974b27204101bf
pgoeri@chello.at <pgoeri@chello.at@c7234ad4-e415-7ad9-c24d-b110a3a82092>
2012-04-07 22:24:47 +0000

    p.d.irfree: replaced all filehoster with extabit.com & turbobit.net

--- trunk/plugin.download.irfree/resources/lib/IRFreeCore.py
+++ trunk/plugin.download.irfree/resources/lib/IRFreeCore.py
@@ -4,13 +4,35 @@ from DDLScraperCore import DDLScraperCore
 class IRFreeCore(DDLScraperCore):
 	
 	def __init__(self):
+		super(type(self),self).__init__()
+		
 		self.__title__		= "IRFree.com"
 		self.__url__		= "http://www.irfree.com"
+		#self.__url__		= "http://www.irfree.com/tv-shows" # used for filehoster self test
 		self.__nextpage__	= '<div class="turnRight"><a'
 		# prevent dos attack ;), wait at least 400 milliseconds between every fetch
 		# otherwise more than 20 posts per page doesn't work
 		self.__delay__ = 0.4
 	
+		self.__filehoster__	= {}
+		self.__filehoster__["0"] =	{ "url": "http://turbobit.net" }
+		self.__filehoster__["1"] =	{ "url": "http://extabit.com" }
+		#self.__filehoster__[""] =	{ "url": "" }
+		
+		# ignore the following urls, when looking for new filehosters
+		self.__urls__		= []
+		self.__urls__.append("http://www.irfree.com")
+		self.__urls__.append("http://irfree.net")
+		self.__urls__.append("http://www.tv.com")
+		self.__urls__.append("http://www.tvrage.com")
+		self.__urls__.append("http://www.trutv.com")
+		self.__urls__.append("http://www.colbertnation.com")
+		self.__urls__.append("http://www.travelchannel.com")
+		self.__urls__.append("http://www.wnetwork.com")
+		self.__urls__.append("http://tviv.org")
+		self.__urls__.append("http://uk.eonline.com")
+		#self.__urls__.append("")
+		
 	#===============================================================================
 	#
 	# IRFree specific functions
@@ -23,14 +45,15 @@ class IRFreeCore(DDLScraperCore):
 		if (website == None):
 			website = self._executeRE('Type The Password(.+?)Tags: ', full_website)
 		if (website == None):
-			website = self._executeRE('Download:(.+?)Tags: ', full_website)
+			website = self._executeRE('Download(.+?)Tags: ', full_website)
 		if (website == None):
-			website = self._executeRE('Download:(.+?)<p class="postmetacat"', full_website)
+			website = self._executeRE('Download(.+?)<p class="postmetacat"', full_website)
 		if (website == None):
-			website = self._executeRE('Download:(.+?)class="ratingBox"', full_website)
+			website = self._executeRE('Download(.+?)class="ratingBox"', full_website)
 		if (website == None):
 			# not possible to extract link section, use the whole website for scraping
 			website = full_website
+		
 		return website;
 	
 	def _extractPosts(self, link, website):
@@ -39,21 +62,21 @@ class IRFreeCore(DDLScraperCore):
 		# 3: img
 		# 4: description
 		posts = re.compile('class="newsTitle">.*?<a href="(.+?)".*?>(.+?)</a>.+?(?:<img.+?src="(.+?)".+?</p>|</p>)(.+?)<ul class="newsActions">',re.DOTALL).findall(website)
+		
+		# ignore general posts
+		for post in posts:
+			if (post[0] == "http://www.irfree.com/off-topic/45091-download-files-from-our-private-server.html"):
+				posts.remove(post)
+		
 		return posts
 	
-	def _scrapeFilehosterLinksNow( self, website, filehoster ):
-		if (filehoster == 0):
-			url = "http://hotfile.com/dl/"
-		elif (filehoster == 1):
-			url = "http://www.filesonic.com/file/"
-		elif (filehoster == 2):
-			url = "http://www.fileserve.com/file/"
-		elif (filehoster == 3):
-			url = "http://upfile.in"
-		elif (filehoster == 4):
-			url = "http://www.wupload.com"
-		else:
-			return [ ]
+	def _scrapeFilehosterLinksNow( self, website, fh_key ):
+		
+		fh = self._getFilehoster(fh_key)
+		if (fh == None):
+			return []
+		
+		url = fh["url"]
 		
 		if self.__dbg__:
 			print self.__plugin__ + " _scrapeFilehosterLinksNow: starting with " + repr(url)
@@ -76,11 +99,11 @@ class IRFreeCore(DDLScraperCore):
 		return file_links
 	
 	def _scrapeFilehosterLinks( self, website):
-		file_links = self._scrapeFilehosterLinksNow( website, int(self.__addon__.getSetting( "filehoster" )) )
+		file_links = self._scrapeFilehosterLinksNow( website, self.__addon__.getSetting( "filehoster" ) )
 		if ( len(file_links) == 0):
-			alt_filehoster = int(self.__addon__.getSetting( "alt_filehoster" ))
-			if ( alt_filehoster != 0):
-				file_links = self._scrapeFilehosterLinksNow( website, (alt_filehoster-1) )
+			alt_filehoster = self.__addon__.getSetting( "alt_filehoster" )
+			if ( alt_filehoster != "-1"):
+				file_links = self._scrapeFilehosterLinksNow( website, alt_filehoster )
 		
 		# remove duplicates
 		file_links = set(file_links)
@@ -89,7 +112,7 @@ class IRFreeCore(DDLScraperCore):
 	
 	def _scrapeFilehoster( self, website):
 		# extract links
-		urls = re.compile('href="(http://.+?)/').findall(website)
+		urls = re.compile('href="(http://.+?)["/]').findall(website)
 		return set(urls)
 	
 	def _getCategories(self):
