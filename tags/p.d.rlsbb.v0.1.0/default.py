c7800eca13cd550c9cf5041c949f10f8a75a0a27
pgoeri85@gmail.com <pgoeri85@gmail.com@c7234ad4-e415-7ad9-c24d-b110a3a82092>
2012-06-25 22:09:49 +0000

    tagged p.d.rlsbb v0.1.0

---
+++ tags/p.d.rlsbb.v0.1.0/default.py
@@ -0,0 +1,23 @@
+import sys, os, xbmc, xbmcaddon
+
+# plugin constants
+__version__			= "0.1.0"
+__plugin__			= "RlsBB.me-" + __version__
+__addonID__			= "plugin.download.rlsbb"
+__author__			= "pgoeri"
+__url__				= "http://pgoeri-xbmc-plugins.googlecode.com"
+__svn_url__			= "http://pgoeri-xbmc-plugins.googlecode.com/svn/trunk/plugin.download.rlsbb/"
+__XBMC_Revision__	= "11.0" # Eden
+__date__			= "23-06-2012"
+
+__addon__			= xbmcaddon.Addon(id=__addonID__)
+__language__		= __addon__.getLocalizedString
+__dbg__				= __addon__.getSetting( "debug" ) == "true"
+
+
+if (__name__ == "__main__" ):
+	sys.path.append( os.path.join( __addon__.getAddonInfo('path'), "resources", "lib" ) )
+	
+	import RlsBBNavigation as navigation
+	
+	navigation.RlsBBNavigation().navigate()
